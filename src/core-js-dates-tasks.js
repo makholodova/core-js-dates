77e2c442d27e22899dce9efa164df8fe98efdaac
Marina Kholodova <marinaholodova@gmail.com>
2025-02-24 23:07:15 +0300

    feat: done getCountDaysInMonth task

--- src/core-js-dates-tasks.js
+++ src/core-js-dates-tasks.js
@@ -79,8 +79,9 @@ function getNextFriday(/* date */) {
  * 1, 2024 => 31
  * 2, 2024 => 29
  */
-function getCountDaysInMonth(/* month, year */) {
-  throw new Error('Not implemented');
+function getCountDaysInMonth(month, year) {
+  const resDate = new Date(year, month, 0);
+  return resDate.getDate();
 }
 
 /**
