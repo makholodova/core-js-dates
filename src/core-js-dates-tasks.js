94688d271e0081146b87d57a2f6053425e2e6e6f
Marina Kholodova <marinaholodova@gmail.com>
2025-02-25 11:19:47 +0300

    feat: done getQuarter task

--- src/core-js-dates-tasks.js
+++ src/core-js-dates-tasks.js
@@ -202,8 +202,9 @@ function getNextFridayThe13th(/* date */) {
  * Date(2024, 5, 1) => 2
  * Date(2024, 10, 10) => 4
  */
-function getQuarter(/* date */) {
-  throw new Error('Not implemented');
+function getQuarter(date) {
+  const month = date.getMonth();
+  return Math.ceil((month + 1) / 3);
 }
 
 /**
